6613ec214ff7a4c872361223ca14c0c2ab894e5b
Eleni-Ke <97251568+Eleni-Ke@users.noreply.github.com>
2022-11-29 16:37:36 +0100

    Homework and extra homework js D7

---
+++ extra.js
@@ -0,0 +1,128 @@
+// Additional assignments for Day 3
+
+/*EX 1
+ Use a ternary operator to assign to a variable called gender the string values "male" or "female".
+ The choice should be made based on the value of another variable called isMale.
+*/
+
+let isMale = true;
+let gender;
+
+isMale ? (gender = "male") : (gender = "female");
+console.log("The gender is: ", gender);
+
+/*EX 2
+ Write a piece of code for checking if, given two integers, 
+ the value of one of them is 8 or if their addition or subtraction is equal to 8.
+*/
+
+let firstInteger = 12;
+let secondInteger = 20;
+let isOneInterger8 = (firstInteger === 8) || (secondInteger ===8);
+let isSumEqual8 = ((firstInteger + secondInteger) === 8) || ((firstInteger - secondInteger) === 8) || ((secondInteger - firstInteger) === 8);
+
+if (isOneInterger8 || isSumEqual8) {
+    console.log("Yes it's true");
+} else {
+    console.log( "No it's false");
+}
+
+/*EX 3
+ Create a variable and assign to it the concatenation of two strings.
+*/
+
+let thisIsAConcatenation = "I am on half " + "and I am the other half";
+console.log(thisIsAConcatenation);
+
+/*EX 4
+ Create three variables and assign a numerical value to each one of them. 
+ Using a conditional statement, write a piece of code for sorting their values from highest to lowest.
+ Display the result in the console.
+*/
+
+let firstVar = 22;
+let secondVar = 1;
+let thirdVar = 15;
+
+let highestVar;
+let middleVar;
+let lowestVar;
+
+if ((firstVar >= secondVar) && (firstVar >= thirdVar)) {
+    highestVar = firstVar;
+} else if ((firstVar >= secondVar) || (firstVar >= thirdVar)) {
+    middleVar = firstVar;
+} else {
+    lowestVar = firstVar;
+}
+
+if ((secondVar >= firstVar) && (secondVar >= thirdVar)) {
+    highestVar = secondVar;
+} else if ((secondVar >= firstVar) || (secondVar >= thirdVar)) {
+    middleVar = secondVar;
+} else {
+    lowestVar = secondVar;
+}
+
+if ((thirdVar >= firstVar) && (thirdVar >= secondVar)) {
+    highestVar = thirdVar;
+} else if ((thirdVar >= firstVar) || (thirdVar >= secondVar)) {
+    middleVar = thirdVar;
+} else {
+    lowestVar = thirdVar;
+}
+
+console.log("The highest variable is: ", highestVar);
+console.log("The middle variable is: ", middleVar);
+console.log("The lowest variable is: ", lowestVar);
+
+/*EX 5
+ Write a piece of code for finding the average of two given integers.
+*/
+
+let givenInteger1 = 4;
+let givenInteger2 = 8;
+
+let findingAverage = (givenInteger1 + givenInteger2) / 2;
+console.log(findingAverage);
+
+/*EX 6
+ Write a piece of code for finding the longest of two given strings.
+*/
+
+let string1 = "I am the first string.";
+let string2 = "I am the second string.";
+let longestString;
+
+if (string1.length > string2.length) {
+    longestString = string1;
+} else if (string1.length < string2.length) {
+    longestString = string2;
+} else {
+    longestString = "They have the same length.";
+}
+
+console.log("The longest string is: ", longestString);
+
+/*EX 7
+ Write a piece of code for checking if a given value is a integer or not.
+*/
+
+let maybeAnInteger = 1.32;
+
+(maybeAnInteger % 1) === 0 ? (console.log("It is an integer")) : (console.log("It is not an integer..."));
+
+/*EX 8
+ Write a piece of code for calculating a certain percentage of a given number.
+ (Ex.: the 20% of 400 is 80)
+*/
+
+let randomNumber = 40;
+let tenPercentOfNumber = randomNumber * 0.1;
+console.log(tenPercentOfNumber);
+
+/*EX 9
+ Write a piece of code for checking if a given number is even or odd.
+*/
+
+(randomNumber % 2) === 0 ? (console.log("The random number is even")) : (console.log("The random number is odd."));
---
+++ index.js
@@ -0,0 +1,95 @@
+/*
+ASSIGNMENT RULES
+- All the answers must be written in JavaScript
+- You can ask for help, reach the Teaching Assistants if needed
+- You can Google / use StackOverflow BUT only when you think you need something we didn't cover during lessons yet
+- You can test your code in a separate file or de-commenting the single exercises in this one.
+- You can use the bash terminal, the VSCode terminal or the one embedded in your Operating System if you're using macOS or Linux.
+- The solution must be available for the tutors by the end of the day (5PM CET)
+*/
+
+/* EXERCISE 1
+ Enumerate and describe the main datatypes in JavaScript. Try to explain the concepts as you were speaking to a 12 years old.
+*/
+
+
+/* 1) Numbers: Numbers in js are like numbers in maths class that you can count. They can go from a very high number in the negatives, to a very high number in the positives. They can be interger or floating. Sometime you might find an impossible one, like 23/0 for example. The computer will return NaN, which stands for not a number. Although it technically is.
+2) Strings: Strings in js are usually words or sentences, but they can contain numbers too. The computer will use the string as one and render it exactly that way. It has to be put in quotes.
+3) Undefined: Undefined means that the computer doesn't know what the variable contains, because nobody told him yet.
+4) Null: Null means that the variable is empty.
+5) Boolean: Boolean can be either true or false.
+6) BigInt: Big int are numbers even higher than regular numbers. We will probably never need them.
+7) Symbols: We will probably never need symbols either.*/
+
+/* EXERCISE 2
+ Try to describe what a variable is, in your own words.
+*/
+
+/* A variable is like a container that holds a specific type of data. We have to create it by declaring it with let and then add it's name. It's content can be changed, added or deleted.*/
+
+/* EXERCISE 3
+ Write the code to execute an addition (a sum) of the numbers 12 and 20.
+*/
+
+let executeAddition = 12 + 20;
+console.log("This is the result of executing the addition of 12 and 20:", executeAddition);
+
+/* EXERCISE 4
+ Create a variable named x and assign to it the number 12.
+*/
+
+let x = 12;
+console.log(x);
+
+/* EXERCISE 5
+ Create a variable called name and assign to it the string "John Doe".
+*/
+
+let name = "John Doe";
+console.log(name);
+
+//VSCode is telling be that the variable name "name" is depricated. It shouldn't be used. It can still be printed.
+
+/* EXERCISE 6
+ Execute a subtraction between the number 12 and the variable x you declared before (which is storing the value 12).
+*/
+
+let substracting12FromX = x - 12;
+console.log(substracting12FromX);
+
+/* EXERCISE 7
+ Create two variables: name1 and name2. Assign to name1 the string "john", and to name2 the string "John" (this one has a capital J!)
+ Verify that name1 is different from name2 (HINT: check their equality to be false).
+ Finally, verify that the equality between name1 and name2 becomes true if both are lowercase (without changing the value of name2).
+*/
+
+let name1 = "john";
+let name2 = "John";
+let compareNames = name1 === name2;
+console.log(compareNames);
+
+let compareNamesInLowerCases = name1.toLowerCase() === name2.toLocaleLowerCase();
+console.log(compareNamesInLowerCases);
+
+/* EXERCISE 8
+ Create a variable named x, and assign to it a numeric value less than 10.
+ Write the code to print the literal value of x (ex.: if x is 1, it should print "one", if x is 5 it should print "five" etc.).
+*/
+
+x = 6; 
+//I can't assign the same name to another variable, so I reused the variable from exercise 4. Recycling variable :D
+if (x===6){
+    console.log("six");
+    }
+
+/* EXERCISE 9
+ [Extra] Assign a value to a variable using a ternary operator (this topic has not been covered during lesson yet,
+ try to search it by yourself, tomorrow morning we'll discuss it together!)
+*/
+
+let height = 172;
+let ridingRollerCoaster;
+
+height >= 150 ? (ridingRollerCoaster = "You can ride the roller coaster!") : (ridingRollerCoaster = "You cannot ride the roller coaster.");
+
+console.log(ridingRollerCoaster);
